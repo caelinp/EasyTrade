42c2422f83202439fb5cafa8025c1a79fccb350e
caelinp <cpbluewhale@gmail.com>
2023-10-30 16:22:19 -0700

    added basic view jobs page

--- pages/view-jobs.tsx
+++ pages/view-jobs.tsx
@@ -1,9 +1,11 @@
 // pages/view-jobs.tsx
 import React, { useState, useEffect } from 'react';
+import styles from './view-jobs.module.css';
+
 
 const PROFESSIONS = ['Electrician', 'Carpenter', 'Locksmith', 'Plumber', 'Landscaper', 'Tiler', 'HVAC Technician', 'Painter', 'Flooring Specialist', 'Roofer'];
 
-const JOBS = [
+const FAKE_JOBS = [
   { id: 1, title: 'Electrician needed', city: 'Los Angeles', description: 'Fix some wires', datePosted: '2023-10-30', posterFirstName: 'John', skills: ['Electrician'] },
   { id: 2, title: 'Carpenter for furniture', city: 'New York', description: 'Build a table and chairs', datePosted: '2023-10-29', posterFirstName: 'Jane', skills: ['Carpenter'] },
   { id: 3, title: 'Locksmith assistance', city: 'San Francisco', description: 'Change home locks', datePosted: '2023-10-28', posterFirstName: 'Alex', skills: ['Locksmith'] },
@@ -16,59 +18,57 @@ const JOBS = [
   { id: 10, title: 'Roofing repair', city: 'Denver', description: 'Fix a leak in the roof', datePosted: '2023-10-05', posterFirstName: 'Tom', skills: ['Roofer'] }
 ];
 
+const ViewJobs = () => {
+  const [cityFilter, setCityFilter] = useState('');
+  const [skillFilter, setSkillFilter] = useState([]);
+  const [keywordFilter, setKeywordFilter] = useState('');
 
-const ViewJobsPage = () => {
-  const [filters, setFilters] = useState({ city: '', skills: [], keywords: '' });
-  const [filteredJobs, setFilteredJobs] = useState(JOBS);
-  const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
-
-  useEffect(() => {
-    const results = JOBS.filter(job => 
-      job.city.includes(filters.city) && 
-      (selectedSkills.length === 0 || selectedSkills.some(skill => job.skills.includes(skill))) &&
-      (job.title.includes(filters.keywords) || job.description.includes(filters.keywords))
+  const filteredJobs = FAKE_JOBS.filter(job => {
+    return (
+      job.city.includes(cityFilter) &&
+      skillFilter.every(skill => job.skills.includes(skill)) &&
+      (job.title.includes(keywordFilter) || job.description.includes(keywordFilter))
     );
-    setFilteredJobs(results);
-  }, [filters, selectedSkills]);
+  });
 
   return (
-    <div>
-      <div>
-        <input placeholder="City" onChange={(e) => setFilters(prev => ({ ...prev, city: e.target.value }))} />
-        
-        <div>
-          <select 
-            onChange={(e) => {
-              if (!selectedSkills.includes(e.target.value)) {
-                setSelectedSkills([...selectedSkills, e.target.value]);
-              }
-            }}>
-            {PROFESSIONS.map(skill => (
-              <option key={skill} value={skill}>{skill}</option>
-            ))}
-          </select>
-          <ul>
-            {selectedSkills.map(skill => (
-              <li key={skill}>
-                {skill}
-                <button onClick={() => setSelectedSkills(prev => prev.filter(s => s !== skill))}>X</button>
-              </li>
-            ))}
-          </ul>
+    <div className={styles.container}>
+      <div className={styles.filters}>
+        <div className={styles.filter}>
+          <input 
+            type="text" 
+            placeholder="City" 
+            value={cityFilter} 
+            onChange={e => setCityFilter(e.target.value)} 
+          />
         </div>
-
-        <input placeholder="Keywords" onChange={(e) => setFilters(prev => ({ ...prev, keywords: e.target.value }))} />
+        <div className={styles.filter}>
+          <select multiple value={skillFilter} onChange={e => setSkillFilter([...e.target.options].filter(o => o.selected).map(o => o.value))}>
+            {PROFESSIONS.map(skill => <option key={skill} value={skill}>{skill}</option>)}
+          </select>
+        </div>
+        <div className={styles.filter}>
+          <input 
+            type="text" 
+            placeholder="Keywords" 
+            value={keywordFilter} 
+            onChange={e => setKeywordFilter(e.target.value)} 
+          />
+        </div>
       </div>
-
-      <div>
+      <div className={styles.jobs}>
         {filteredJobs.map(job => (
-          <div key={job.id}>
-            <h2>{job.title}</h2>
-            <p>{job.city}</p>
-            <p>{job.description}</p>
-            <p>{job.datePosted}</p>
-            <p>Posted by: {job.posterFirstName}</p>
-            <p>Skills: {job.skills.join(', ')}</p>
+          <div key={job.id} className={styles.jobCard}>
+            <h3 className={styles.jobTitle}>{job.title}</h3>
+            <p className={styles.jobCity}>City: {job.city}</p>
+            <p className={styles.jobDescription}>{job.description}</p>
+            <p className={styles.jobDate}>Date posted: {job.datePosted}</p>
+            <p className={styles.jobPoster}>Posted by: {job.posterFirstName}</p>
+            <div className={styles.jobSkills}>
+              {job.skills.map(skill => (
+                <span key={skill} className={styles.skillItem}>{skill}</span>
+              ))}
+            </div>
           </div>
         ))}
       </div>
@@ -76,4 +76,4 @@ const ViewJobsPage = () => {
   );
 };
 
-export default ViewJobsPage;
+export default ViewJobs;
