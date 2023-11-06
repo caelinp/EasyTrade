ce2fa86112175795a47aea3fbf79e708a319891c
caelinp <cpbluewhale@gmail.com>
2023-11-06 15:21:21 -0800

    updating next config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Add any other Next.js configurations you might need here
+  
+    // This will enable static HTML export of your app
+    // Replace `<your_base_path>` with the path you want to use for GitHub Pages, if any
+    // For example, if your GitHub username is `john-doe` and your repository is `my-app`,
+    // your base path should be '/my-app'
+    basePath: '/EasyTrade',
+    output: 'standalone',
+  };
+  
