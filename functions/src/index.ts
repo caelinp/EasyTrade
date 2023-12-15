4da6124191caa6551d026af36c23ce2b110447f7
caelinp <cpbluewhale@gmail.com>
2023-12-14 16:34:31 -0800

    job board entries are now from the database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import * as express from "express";
 import * as cors from "cors";
 
 admin.initializeApp();
+const NUM_LEADS_INITIAL = 5;
 
 interface JobSchema {
   address: string;
@@ -21,6 +22,8 @@ interface JobSchema {
   skills: string[];
   title: string;
   timestamp: string;
+  numLeadsTotal: string;
+  numLeadsPurchased: string;
 }
 
 const jobSchema: JobSchema = {
@@ -39,21 +42,25 @@ const jobSchema: JobSchema = {
   skills: [],
   title: "",
   timestamp: "",
+  numLeadsTotal: "",
+  numLeadsPurchased: ""
 };
 
 // Job summary interface excludes properties that will be available if leads are purchased
 interface JobSummarySchema {
-    city: string;
-    country: string;
-    description: string;
-    duration: string;
-    budget: string;
-    firstName: string;
-    provinceState: string;
-    skills: string[];
-    title: string;
-    timestamp: string;
-  }
+  city: string;
+  country: string;
+  description: string;
+  duration: string;
+  budget: string;
+  firstName: string;
+  provinceState: string;
+  skills: string[];
+  title: string;
+  timestamp: string;
+  numLeadsTotal: string;
+  numLeadsPurchased: string;
+}
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -71,6 +78,8 @@ app.post("/addJob", async (req, res) => {
 
   const now = new Date();
   jobData.timestamp = now.toISOString();
+  jobData.numLeadsTotal = (NUM_LEADS_INITIAL).toString();
+  jobData.numLeadsPurchased = "0";
 
   try {
     const writeResult = await admin.firestore().collection("jobs").add(jobData);
@@ -83,29 +92,86 @@ app.post("/addJob", async (req, res) => {
 });
 
 app.get("/getJobs", async (req, res) => {
-    try {
-      const jobsSnapshot = await admin.firestore().collection("jobs").get();
-      const jobs = jobsSnapshot.docs.map(doc => {
-        const jobData = doc.data() as JobSchema;
-        const jobSummary: JobSummarySchema = {
-          city: jobData.city,
-          country: jobData.country,
-          description: jobData.description,
-          duration: jobData.duration,
-          budget: jobData.budget,
-          firstName: jobData.firstName,
-          provinceState: jobData.provinceState,
-          skills: jobData.skills,
-          title: jobData.title,
-          timestamp: jobData.timestamp,
-        };
-        return { id: doc.id, ...jobSummary };
-      });
-      res.status(200).send(jobs);
-    } catch (error) {
-      functions.logger.error("Error getting jobs", error);
-      res.status(500).send({ error: "Error getting jobs" });
+  try {
+    const limit = parseInt(req.query.limit as string) || 10;
+    const startAfter = req.query.startAfter as string;
+
+    // Filter parameters
+    const keywords = req.query.keywords as string;
+    const city = req.query.city as string;
+    const skills = req.query.skills as string[]
+    const minDuration = parseInt(req.query.minDuration as string);
+    const maxDuration = parseInt(req.query.maxDuration as string);
+    const daysSincePosted = parseInt(req.query.daysSincePosted as string);
+
+    let query = admin.firestore().collection("jobs").orderBy("timestamp");
+
+    // Apply filters
+    if (keywords) {
+      query = query.where('title', 'array-contains-any', keywords.split(' '))
+        .where('description', 'array-contains-any', keywords.split(' '));
     }
-  });
+    if (city) {
+      query = query.where('city', '==', city);
+    }
+    if (skills) {
+      skills.forEach(skill => {
+        query = query.where('skills', 'array-contains', skill.trim());
+      });
+    }
+    if (!isNaN(minDuration) && !isNaN(maxDuration)) {
+      query = query.where('duration', '>=', minDuration)
+        .where('duration', '<=', maxDuration);
+    }
+    if (!isNaN(daysSincePosted)) {
+      const dateLimit = admin.firestore.Timestamp.fromDate(new Date(Date.now() - daysSincePosted * 24 * 60 * 60 * 1000));
+      query = query.where('timestamp', '>=', dateLimit);
+    }
+
+    if (startAfter) {
+      const lastDoc = await admin.firestore().collection("jobs").doc(startAfter).get();
+      if (!lastDoc.exists) {
+        res.status(404).send({ error: "Invalid 'startAfter' document ID" });
+        return;
+      }
+      query = query.startAfter(lastDoc);
+    }
+
+    query = query.limit(limit);
+    const jobsSnapshot = await query.get();
+    const jobs = jobsSnapshot.docs.map(doc => {
+      const jobData = doc.data() as JobSchema;
+      const jobSummary: JobSummarySchema = {
+        city: jobData.city,
+        country: jobData.country,
+        description: jobData.description,
+        duration: jobData.duration,
+        budget: jobData.budget,
+        firstName: jobData.firstName,
+        provinceState: jobData.provinceState,
+        skills: jobData.skills,
+        title: jobData.title,
+        timestamp: jobData.timestamp,
+        numLeadsTotal: jobData.numLeadsTotal,
+        numLeadsPurchased: jobData.numLeadsPurchased
+      };
+      return { id: doc.id, ...jobSummary };
+    });
+
+    if (jobs.length === 0) {
+      res.status(404).send({ error: "No jobs found" });
+      return;
+    }
+
+    const lastVisible = jobsSnapshot.docs[jobsSnapshot.docs.length - 1];
+    res.status(200).send({
+      jobs,
+      nextPageToken: lastVisible ? lastVisible.id : null
+    });
+  } catch (error) {
+    functions.logger.error("Error getting jobs", error);
+    res.status(500).send({ error: "Error getting jobs" });
+  }
+});
 
 export const api = functions.https.onRequest(app);
--- pages/view-jobs.tsx
+++ pages/view-jobs.tsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import styles from './ViewJobs.module.css';
 
+const DEFAULT_RESULT_LIMIT = 2;
+
 interface Job {
   id: number;
   title: string;
   city: string;
   description: string;
-  datePosted: string;
+  datePosted: Date;
   posterFirstName: string;
   skills: string[];
   estimatedDuration: string;
+  estimatedBudget: string; 
+  numLeadsTotal: string;
+  numLeadsPurchased: string;
 }
 
 const PROFESSIONS: string[] = [
-  'Electrician', 'Carpenter', 'Locksmith', 'Plumber', 'Landscaper', 
+  'Electrician', 'Carpenter', 'Locksmith', 'Plumber', 'Landscaper',
   'Tiler', 'HVAC Technician', 'Painter', 'Flooring Specialist', 'Roofer'
 ];
 
@@ -30,105 +35,135 @@ const ESTIMATED_DURATIONS: string[] = [
 ];
 
 const FAKE_JOBS: Job[] = [
-  { 
-    id: 1, 
-    title: 'Electrician needed', 
-    city: 'Los Angeles', 
-    description: 'Fix some wires', 
-    datePosted: '2023-10-30', 
-    posterFirstName: 'John', 
+  {
+    id: 1,
+    title: 'Electrician needed',
+    city: 'Los Angeles',
+    description: 'Fix some wires',
+    datePosted: new Date('2023-10-30'),
+    posterFirstName: 'John',
     skills: ['Electrician'],
-    estimatedDuration: '2-3 days'
+    estimatedDuration: '2-3 days',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 2, 
-    title: 'Carpenter for furniture', 
-    city: 'New York', 
-    description: 'Build a table and chairs', 
-    datePosted: '2023-10-29', 
-    posterFirstName: 'Jane', 
+  {
+    id: 2,
+    title: 'Carpenter for furniture',
+    city: 'New York',
+    description: 'Build a table and chairs',
+    datePosted: new Date('2023-10-29'),
+    posterFirstName: 'Jane',
     skills: ['Carpenter'],
-    estimatedDuration: '1-2 weeks'
+    estimatedDuration: '1-2 weeks',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 3, 
-    title: 'Locksmith assistance', 
-    city: 'San Francisco', 
-    description: 'Change home locks', 
-    datePosted: '2023-10-28', 
-    posterFirstName: 'Alex', 
+  {
+    id: 3,
+    title: 'Locksmith assistance',
+    city: 'San Francisco',
+    description: 'Change home locks',
+    datePosted: new Date('2023-10-28'),
+    posterFirstName: 'Alex',
     skills: ['Locksmith'],
-    estimatedDuration: '1 day'
+    estimatedDuration: '1 day',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 4, 
-    title: 'Urgent plumbing work', 
-    city: 'Chicago', 
-    description: 'Fix a leaky faucet', 
-    datePosted: '2023-10-25', 
-    posterFirstName: 'Marie', 
+  {
+    id: 4,
+    title: 'Urgent plumbing work',
+    city: 'Chicago',
+    description: 'Fix a leaky faucet',
+    datePosted: new Date('2023-10-25'),
+    posterFirstName: 'Marie',
     skills: ['Plumber'],
-    estimatedDuration: '3-4 days'
+    estimatedDuration: '3-4 days',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 5, 
-    title: 'Landscaping the garden', 
-    city: 'Los Angeles', 
-    description: 'Design a garden landscape', 
-    datePosted: '2023-10-22', 
-    posterFirstName: 'Luke', 
+  {
+    id: 5,
+    title: 'Landscaping the garden',
+    city: 'Los Angeles',
+    description: 'Design a garden landscape',
+    datePosted: new Date('2023-10-22'),
+    posterFirstName: 'Luke',
     skills: ['Landscaper'],
-    estimatedDuration: 'less than 1 month'
+    estimatedDuration: 'less than 1 month',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 6, 
-    title: 'Tiling the bathroom', 
-    city: 'San Diego', 
-    description: 'Tile walls and floor', 
-    datePosted: '2023-10-20', 
-    posterFirstName: 'Eva', 
+  {
+    id: 6,
+    title: 'Tiling the bathroom',
+    city: 'San Diego',
+    description: 'Tile walls and floor',
+    datePosted: new Date('2023-10-20'),
+    posterFirstName: 'Eva',
     skills: ['Tiler'],
-    estimatedDuration: '2-3 weeks'
+    estimatedDuration: '2-3 weeks',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 7, 
-    title: 'HVAC maintenance', 
-    city: 'Houston', 
-    description: 'Check and maintain HVAC system', 
-    datePosted: '2023-10-18', 
-    posterFirstName: 'Sarah', 
+  {
+    id: 7,
+    title: 'HVAC maintenance',
+    city: 'Houston',
+    description: 'Check and maintain HVAC system',
+    datePosted: new Date('2023-10-18'),
+    posterFirstName: 'Sarah',
     skills: ['HVAC Technician'],
-    estimatedDuration: 'less than 1 week'
+    estimatedDuration: 'less than 1 week',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 8, 
-    title: 'Home painting', 
-    city: 'Seattle', 
-    description: 'Paint interior walls', 
-    datePosted: '2023-10-15', 
-    posterFirstName: 'Jake', 
+  {
+    id: 8,
+    title: 'Home painting',
+    city: 'Seattle',
+    description: 'Paint interior walls',
+    datePosted: new Date('2023-10-15'),
+    posterFirstName: 'Jake',
     skills: ['Painter'],
-    estimatedDuration: '1-2 months'
+    estimatedDuration: '1-2 months',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 9, 
-    title: 'Flooring work', 
-    city: 'Dallas', 
-    description: 'Install wooden floors', 
-    datePosted: '2023-10-10', 
-    posterFirstName: 'Alice', 
+  {
+    id: 9,
+    title: 'Flooring work',
+    city: 'Dallas',
+    description: 'Install wooden floors',
+    datePosted: new Date('2023-10-10'),
+    posterFirstName: 'Alice',
     skills: ['Flooring Specialist'],
-    estimatedDuration: '2+ months'
+    estimatedDuration: '2+ months',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   },
-  { 
-    id: 10, 
-    title: 'Roofing repair', 
-    city: 'Denver', 
-    description: 'Fix a leak in the roof', 
-    datePosted: '2023-10-05', 
-    posterFirstName: 'Tom', 
+  {
+    id: 10,
+    title: 'Roofing repair',
+    city: 'Denver',
+    description: 'Fix a leak in the roof',
+    datePosted: new Date('2023-10-05'),
+    posterFirstName: 'Tom',
     skills: ['Roofer'],
-    estimatedDuration: '2-3 days'
+    estimatedDuration: '2-3 days',
+    estimatedBudget: '100',
+    numLeadsTotal: "5",
+    numLeadsPurchased: "4"
   }
 ];
 
@@ -178,24 +213,77 @@ const ViewJobs: React.FC = () => {
   const [dateFilter, setDateFilter] = useState<string>('allTime');
   const [minDuration, setMinDuration] = useState<string>('Any');
   const [maxDuration, setMaxDuration] = useState<string>('Any');
-  
+
   const [minDurationOptions, setMinDurationOptions] = useState<string[]>(['Any', ...ESTIMATED_DURATIONS]);
   const [maxDurationOptions, setMaxDurationOptions] = useState<string[]>(['Any', ...ESTIMATED_DURATIONS]);
+  const [moreResultsVisible, setMoreResultsVisible] = useState(true);
+  const [nextPageToken, setNextPageToken] = useState(null)
+  const [resultsLimit, setResultsLimit] = useState(DEFAULT_RESULT_LIMIT)
+  const [jobsShown, setJobsShown] = useState<Job[]>([])
 
-  useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        const response = await fetch('https://us-central1-easytrade-bdab6.cloudfunctions.net/api/getJobs');
-        if (!response.ok) {
+  const populateJobBoard = (jobs: any) => {
+    let jobObjects: Job[]= []
+    for (const job of jobs) {
+      const jobObject: Job = 
+      {
+        id: job.id,
+        title: job.title,
+        city: job.city,
+        description: job.description,
+        datePosted: new Date(job.timestamp),
+        posterFirstName: job.firstName,
+        skills: job.skills,
+        estimatedDuration: job.duration,
+        estimatedBudget: job.budget,
+        numLeadsTotal: job.numLeadsTotal,
+        numLeadsPurchased: job.numLeadsPurchased
+      }
+      jobObjects.push(jobObject)
+    }
+    setJobsShown(jobsShown.concat(jobObjects))
+    
+  }
+
+  const fetchJobs = async () => {
+    try {
+      const url = new URL('https://us-central1-easytrade-bdab6.cloudfunctions.net/api/getJobs');
+  
+      // Add query parameters if they are provided
+      if (nextPageToken) {
+        url.searchParams.append('startAfter', nextPageToken);
+      }
+      url.searchParams.append('limit', resultsLimit.toString());
+  
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        if (response.status === 404) {
+          console.log("No more results");
+          setMoreResultsVisible(false);
+        } else {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const jobs = await response.json();
-        console.log(jobs); // Log the entire response
-      } catch (error) {
-        console.error("Error getting jobs:", error);
       }
-    };
+  
+      const jobs = await response.json();
+      console.log(jobs);
+  
+      if (jobs.nextPageToken != null) {
+        setNextPageToken(jobs.nextPageToken);
+        setMoreResultsVisible(true);
+      } else {
+        setMoreResultsVisible(false);
+      }
+  
+      populateJobBoard(jobs.jobs);
+  
+    } catch (error) {
+      console.error("Error getting jobs:", error);
+    }
+  };
+  
 
+  useEffect(() => {
     fetchJobs();
   }, []); // Empty dependency array ensures this runs once on mount
 
@@ -247,7 +335,7 @@ const ViewJobs: React.FC = () => {
     setSkillFilter(prev => prev.filter(skill => skill !== skillToRemove));
   };
 
-  const filteredJobs: Job[] = FAKE_JOBS.filter((job: Job) => {
+  const filteredJobs: Job[] = jobsShown.filter((job: Job) => {
     return (
       job.city.toLowerCase().includes(cityFilter.toLowerCase()) &&
       (skillFilter.length === 0 || job.skills.some(skill => skillFilter.includes(skill))) &&
@@ -302,27 +390,27 @@ const ViewJobs: React.FC = () => {
         <div className={styles.filter}>
           <label>Filter by skills needed:</label>
           <select className={styles.skillSelect} onChange={handleSkillSelect} value="">
-              <option value="" disabled>Select a profession</option>
-              <option key={"All"} value="All">All</option>
-              {PROFESSIONS.map(profession => (
+            <option value="" disabled>Select a profession</option>
+            <option key={"All"} value="All">All</option>
+            {PROFESSIONS.map(profession => (
               <option key={profession} value={profession}>
-                  {profession}
+                {profession}
               </option>
-              ))}
+            ))}
           </select>
         </div>
       </div>
       <div className={styles.filtersPanel}>
         <div className={styles.selectedSkills}>
-        {skillFilter.map(skill => (
-              <div key={skill} className={styles.skillItem}>
+          {skillFilter.map(skill => (
+            <div key={skill} className={styles.skillItem}>
               {skill}
               <span className={styles.removeSkill} onClick={() => handleSkillRemove(skill)}>X</span>
-              </div>
-              ))}
+            </div>
+          ))}
         </div>
       </div>
-      <div className={styles.filtersPanel}>  
+      <div className={styles.filtersPanel}>
         {/* Filter by Minimum Estimated Duration */}
         <div className={styles.durationFilters}>
           <label>Filter by estimated duration:</label>
@@ -347,40 +435,45 @@ const ViewJobs: React.FC = () => {
             </div>
           </div>
         </div>
-    </div>
-
-
-
-        {/* Job Cards */}
-        <h1>Filtered Jobs:</h1>
-        <div className={styles.jobs}>
-          {filteredJobs.map(job => (
-            <div key={job.id} className={styles.jobCard}>
-              <h2 className={styles.jobTitle}>{job.title}</h2>
-              <div className={styles.jobDetailsPanels}>
-                <div className={styles.jobDetailsPanelLeft}>
-                  <h3>City:</h3>
-                  <p className={styles.jobCity}>{job.city}</p>
-                  <h3>Date posted:</h3>
-                  <p className={styles.jobDate}>{job.datePosted}</p>
-                  <h3>Posted by:</h3>
-                  <p className={styles.jobPoster}>{job.posterFirstName}</p>
-                  <h3>Estimated Duration:</h3>
-                  <p className={styles.jobDuration}>{job.estimatedDuration}</p>
+      </div>
+      {/* Job Cards */}
+      <h1>Filtered Jobs:</h1>
+      <div className={styles.jobs}>
+        {filteredJobs.map(job => (
+          <div key={job.id} className={styles.jobCard}>
+            <h2 className={styles.jobTitle}>{job.title}</h2>
+            <div className={styles.jobDetailsPanels}>
+              <div className={styles.jobDetailsPanelLeft}>
+                <h3>City:</h3>
+                <p className={styles.jobCity}>{job.city}</p>
+                <h3>Date posted:</h3>
+                <p className={styles.jobDate}>{job.datePosted.toLocaleString('default', { month: 'long' }) + " " + job.datePosted.getDate() + ", " + job.datePosted.getFullYear()}</p>
+                <h3>Posted by:</h3>
+                <p className={styles.jobPoster}>{job.posterFirstName}</p>
+                <h3>Leads Purchased:</h3>
+                <p className={styles.leads}>{job.numLeadsPurchased + " out of " + job.numLeadsTotal}</p>
+              </div>
+              <div className={styles.jobDetailsPanelRight}>
+                <h3>Description:</h3>
+                <p className={styles.jobDescription}>{job.description}</p>
+                <h3>Skills needed:</h3>
+                <div className={styles.jobSkills}>
+                  {job.skills.map(skill => (
+                    <span key={skill} className={styles.skillItem}>{skill}</span>
+                  ))}
                 </div>
-                <div className={styles.jobDetailsPanelRight}>
-                  <h3>Description:</h3>
-                  <p className={styles.jobDescription}>{job.description}</p>
-                  <h3>Skills needed:</h3>
-                  <div className={styles.jobSkills}>
-                    {job.skills.map(skill => (
-                      <span key={skill} className={styles.skillItem}>{skill}</span>
-                    ))}
-                </div>
-                </div>
+                <h3>Estimated Duration:</h3>
+                <p className={styles.jobDuration}>{job.estimatedDuration}</p>
+                <h3>Estimated Budget:</h3>
+                <p className={styles.jobBudget}>{"$" + job.estimatedBudget}</p>
               </div>
             </div>
-          ))}
+          </div>
+        ))}
+      </div>
+      <div className={styles.moreResultsContainer}>
+        {moreResultsVisible && <button className={styles.moreButton} onClick={fetchJobs}>More Results</button>}
+        {!moreResultsVisible && <h2 className={styles.noResults} >No More Results</h2>}
       </div>
     </div>
   );
